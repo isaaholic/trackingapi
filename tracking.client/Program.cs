55c5cd8061c16ef8dd6685a73bc5b399516c1a85
Isa Memmedli <i.a.memmedli@gmail.com>
2022-07-28 21:45:39 +0400

    Add project files.

---
+++ tracking.client/Program.cs
@@ -0,0 +1,31 @@
+﻿using System.Net.Http.Headers;
+using System.Net.Http.Json;
+using tracking.client;
+
+HttpClient client = new();
+client.BaseAddress = new Uri("https://localhost:7229");
+client.DefaultRequestHeaders.Accept.Clear();
+client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+while (true)
+{
+    HttpResponseMessage response = await client.GetAsync("api/issue");
+    response.EnsureSuccessStatusCode();
+
+    if (response.IsSuccessStatusCode)
+    {
+
+        var issues = await response.Content.ReadFromJsonAsync<IEnumerable<IssueDto>>();
+        foreach (var issue in issues)
+        {
+            Console.WriteLine(issue.Title);
+        }
+        Console.WriteLine("Press any key for Reload...");
+        Console.ReadKey();
+    }
+    else
+    {
+        Console.WriteLine("No results");
+    }
+}
+
